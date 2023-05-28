93eabc918d72cb72388f66aabe493620d2fbcb66
grzafnan <grzafnan@gmail.com>
2023-05-29 01:47:49 +0600

    create config and connet mongodb server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
-import dotenv from 'dotenv' 
-dotenv.config()
 import mongoose from "mongoose";
 import app from "./app";
-const PORT: number = Number(process.env.PORT) || 8000;
+import config from "./config/index";
 
 async function run() {
  try {
-  await mongoose.connect('mongodb://127.0.0.1:27017/university-management');
+  await mongoose.connect(config.database_url as string);
   console.log("Database connection established.");
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`)
+  app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}.`)
   })
  } catch (error:any) {
   console.log(error.message);
