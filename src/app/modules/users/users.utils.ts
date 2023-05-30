54769b5a9a65b41a64216e466fce53f663539712
grzafnan <grzafnan@gmail.com>
2023-05-30 23:05:13 +0600

    [DONE] refine userid generate function fron utils

--- src/app/modules/users/users.utils.ts
+++ src/app/modules/users/users.utils.ts
@@ -4,13 +4,11 @@ export const findLastUser = async (): Promise<string | undefined> => {
   const user = await User.findOne({}, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
     .lean()
-  console.log(user?.id)
   return user?.id
 }
 
 export const generateUserId = async (): Promise<string> => {
-  const id = (Number(await findLastUser()) + 1 || '1')
-    .toString()
-    .padStart(5, '0')
-  return id
+  const lastUserId = await findLastUser()
+  const nextId = (Number(lastUserId) + 1 || 1).toString().padStart(5, '0')
+  return nextId
 }
