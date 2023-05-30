6e878129e6b725c367424649375209fd344609e0
grzafnan <grzafnan@gmail.com>
2023-05-30 23:01:25 +0600

    [DONE] fixed user id generate function fron utils

--- src/app/modules/users/users.utils.ts
+++ src/app/modules/users/users.utils.ts
@@ -4,10 +4,13 @@ export const findLastUser = async (): Promise<string | undefined> => {
   const user = await User.findOne({}, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
     .lean()
+  console.log(user?.id)
   return user?.id
 }
 
 export const generateUserId = async (): Promise<string> => {
-  const id = (await findLastUser()) || (0).toString().padStart(5, '0')
+  const id = (Number(await findLastUser()) + 1 || '1')
+    .toString()
+    .padStart(5, '0')
   return id
 }
