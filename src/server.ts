cd6f908b1095cd8912f9dfb2e9dd26fbfa945444
grzafnan <grzafnan@gmail.com>
2023-06-03 01:24:23 +0600

    [DONE] setup logger in application

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,18 @@
+/* eslint-disable no-console */
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { logger, errorLogger } from './shared/logger'
 
 async function run() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database connection established.')
+    logger.info('Database connection established.')
     app.listen(config.port, () => {
-      console.log(`Server is running on port ${config.port}.`)
+      logger.info(`Server is running on port ${config.port}.`)
     })
   } catch (error) {
-    console.log((error as Error).message)
+    errorLogger.error((error as Error).message)
   }
 }
 
