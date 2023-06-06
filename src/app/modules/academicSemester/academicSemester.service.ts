3e126d4ef68d7654abd090151ac8f452b323d05b
grzafnan <grzafnan@gmail.com>
2023-06-07 01:41:56 +0600

    [DONE] Ceate Semester Code Validation in service layer

--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,13 +1,27 @@
+import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
 import { IAcademicSemester } from './academicSemester.interface';
 import AcademicSemester from './academicSemester.model';
+import { academicSemesterTitleCodeMapper } from './academicSemester.constant';
 
 const createAcademicSemester = async (
   academicSemester: IAcademicSemester
 ): Promise<IAcademicSemester | null> => {
+  if (
+    academicSemesterTitleCodeMapper[academicSemester.title] !==
+    academicSemester.code
+  ) {
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Invalid academic semester code!!!'
+    );
+  }
   const createdAcademicSemester = AcademicSemester.create(academicSemester);
   if (!createdAcademicSemester) {
-    throw new ApiError(400, 'Failed to create academic semester!');
+    throw new ApiError(
+      httpStatus.BAD_REQUEST,
+      'Failed to create academic semester!'
+    );
   }
   return createdAcademicSemester;
 };
