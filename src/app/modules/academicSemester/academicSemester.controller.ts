3edcf8ed161e7b1d9c02ae3132ebb273cbf344d4
grzafnan <grzafnan@gmail.com>
2023-06-10 01:44:39 +0600

    [DONE] impelement pagination in all semesters with limit and page

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -3,6 +3,10 @@ import { AcademicSemesterService } from './academicSemester.service';
 import catchAsync from '../../../shared/catchAsync';
 import sendResponse from '../../../shared/sendResponse';
 import httpStatus from 'http-status';
+import pick from '../../../shared/pick';
+import { paginationFields } from '../../../constants/pagination';
+import { IAcademicSemester } from './academicSemester.interface';
+import { IPaginationOptions } from '../../../interfaces/pagination';
 
 const createAcademicSemesterToDB = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -22,6 +26,29 @@ const createAcademicSemesterToDB = catchAsync(
   }
 );
 
+const getAllSemesters = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const paginationOptions: IPaginationOptions = pick(
+      req.query,
+      paginationFields
+    );
+
+    const result = await AcademicSemesterService.getAllSemesters(
+      paginationOptions
+    );
+
+    sendResponse<IAcademicSemester[]>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semesters Retrieved Successfully!',
+      meta: result.meta,
+      data: result.data,
+    });
+    next();
+  }
+);
+
 export const AcademicSemesterController = {
   createAcademicSemesterToDB,
+  getAllSemesters,
 };
