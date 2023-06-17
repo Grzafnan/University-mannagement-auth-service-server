dedef6c0c789a0937616f1213da1a0a0bc6ef253
grzafnan <grzafnan@gmail.com>
2023-06-18 00:41:49 +0600

    [DONE] create dynamic student, faculty, admin and super admin dynamic user ID with enums

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,7 +1,7 @@
 import { Model } from 'mongoose';
 
 export type IUser = {
-  id: string;
+  id: string | undefined;
   password: string;
   role: string;
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -3,14 +3,39 @@ import config from '../../../config';
 import ApiError from '../../../errors/ApiError';
 import { IUser } from './user.interface';
 import User from './user.model';
-import { generateUserId } from './user.utils';
+import {
+  generateAdminId,
+  generateFacultyId,
+  generateStudentId,
+  generateSuperAdminId,
+} from './user.utils';
+import { IAcademicSemester } from '../academicSemester/academicSemester.interface';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
-  // auto generated incremental id
-  user.id = await generateUserId();
+  // generated incremental id
+  const academicSemester: IAcademicSemester = {
+    title: 'Summer',
+    year: '2023',
+    code: '01',
+    startMonth: 'January',
+    endMonth: 'May',
+  };
 
-  // default password
+  if (user.role === ENUM_USER_ROLE.STUDENT) {
+    user.id = await generateStudentId(academicSemester);
+  } else if (user.role === ENUM_USER_ROLE.FACULTY) {
+    user.id = await generateFacultyId();
+  } else if (user.role === ENUM_USER_ROLE.ADMIN) {
+    user.id = await generateAdminId();
+  } else if (user.role === ENUM_USER_ROLE.SUPPER_ADMIN) {
+    user.id = await generateSuperAdminId();
+  } else {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Unknown User type!');
+  }
+
   if (!user.password) {
+    // default password
     user.password = config.default_user_pass as string;
   }
 
