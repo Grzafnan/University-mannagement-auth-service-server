46006709bd2a5dc23de36acc8e3a1a9e9aae06d7
grzafnan <grzafnan@gmail.com>
2023-06-22 01:02:18 +0600

    [DONE] created management department constant, interface and model

---
+++ src/app/modules/management-department/management-department.model.ts
@@ -0,0 +1,44 @@
+import { Schema, model } from 'mongoose';
+import {
+  IManagementDepartment,
+  IManagementDepartmentModel,
+} from './management-department.interface';
+import ApiError from '../../../errors/ApiError';
+import httpStatus from 'http-status';
+
+const managementDepartmentSchema = new Schema<IManagementDepartment>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+managementDepartmentSchema.pre('save', async function (next) {
+  const isExist = await ManagementDepartment.findOne({
+    title: this.title,
+  });
+  if (isExist) {
+    throw new ApiError(
+      httpStatus.CONFLICT,
+      'Management Department is already exists.'
+    );
+  } else {
+    next();
+  }
+});
+
+const ManagementDepartment = model<
+  IManagementDepartment,
+  IManagementDepartmentModel
+>('ManagementDepartment', managementDepartmentSchema);
+
+export default ManagementDepartment;
